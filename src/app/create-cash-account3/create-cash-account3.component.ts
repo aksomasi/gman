1e1d26cfcdf1f6e54c046c0abf783a1ce557c929
Anil Kumar Somasi <animaratha@gmail.com>
2022-04-29 14:06:32 +0530

    modifide screen 3 design

--- src/app/create-cash-account3/create-cash-account3.component.ts
+++ src/app/create-cash-account3/create-cash-account3.component.ts
@@ -12,25 +12,24 @@ export class CreateCashAccount3Component implements OnInit {
   // @ts-ignore
   cashAccountForm1: FormGroup;
   submitted = false;
-
+  totalBusiness!: number;
   constructor(private formBuilder: FormBuilder,
               private cashAccountServiceService: CashAccountServiceService) {}
 
   ngOnInit(): void {
     this.cashAccountForm1 = this.formBuilder.group(
       {
-        companyLegalName: ['', Validators.required],
-        ownerName: ['', Validators.required],
         dealerInfo2018HVACPurchases: ['', Validators.required],
         dealerInfo2018YorkPurchases: ['', Validators.required],
         dealerInfo2018TotalYorkPurchases: ['', Validators.required],
         dealerInfo2019YorkPurchases: ['', Validators.required],
         hvacPrimaryBrand: ['', Validators.required],
         hvacSecondaryBrand: ['', Validators.required],
-        hvacOther: ['', Validators.required],
+        hvacOther: [''],
         businessMixResidentialReplacement: ['', Validators.required],
         businessMixResidentialNewConstruction: ['', Validators.required],
         businessMixCommercial: ['', Validators.required],
+        businessMixTotal: ['', Validators.max(100)],
         purchaseRequired: ['', Validators.required],
         licenseAccept: ['', Validators.required],
         epaCertificationTo: ['', Validators.required],
@@ -49,11 +48,16 @@ export class CreateCashAccount3Component implements OnInit {
   }
   onSubmit(){
     this.submitted = true;
-    const data = this.cashAccountServiceService.getTotalData();
-    data.form1 = this.cashAccountForm1.value;
-    this.cashAccountServiceService.setTotalData(data)
-    const finalData = this.cashAccountServiceService.getTotalData();
-    const totalData = {...finalData.form1, ...finalData.form2,...finalData.form3}
-    console.log(totalData)
+   const controls =  this.cashAccountForm1.controls;
+    this.totalBusiness = +controls['businessMixResidentialReplacement'].value + +controls['businessMixCommercial'].value + +controls['businessMixResidentialNewConstruction'].value
+    if(this.cashAccountForm1.valid && this.totalBusiness === 100) {
+      const data = this.cashAccountServiceService.getTotalData();
+      data.form1 = this.cashAccountForm1.value;
+      this.cashAccountServiceService.setTotalData(data)
+      const finalData = this.cashAccountServiceService.getTotalData();
+      const totalData = {...finalData.form1, ...finalData.form2,...finalData.form3};
+      console.log(totalData);
+    }
+
   }
 }
